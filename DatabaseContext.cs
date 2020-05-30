07365185385fb809d88f0c2e082ba320c4d809e5
Fourtris21 <65542559+Fourtris21@users.noreply.github.com>
2020-05-30 10:44:34 +0300

    Add files via upload

---
+++ DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Data;
+using Data.Classes;
+using System.IO;
+/// <summary>
+/// този глас се грижи за създаването на базата данни с таблиците за events и organisations
+/// </summary>
+namespace Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Organisation> Organisations { get; set; }
+        
+        private readonly string _databasePath;
+        public DatabaseContext(string databasePath)
+        {
+            _databasePath = databasePath;
+            Database.EnsureCreated();
+        }
+        public DatabaseContext()
+        {
+            var dbPath =
+               Path.Combine(System.Environment.GetFolderPath
+               (System.Environment.SpecialFolder.Personal), "eventsDB.db");
+            _databasePath = dbPath;
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename={_databasePath} ");
+        }
+    }
+}

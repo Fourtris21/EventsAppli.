e15368ef477253bcb52a2b4c8694ed01c147a9dd
Fourtris21 <65542559+Fourtris21@users.noreply.github.com>
2020-05-30 10:54:38 +0300

    EventsApp.Android

---
+++ MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics;
+using System.IO;
+using Path = System.IO.Path;
+using SQLite;
+using Data.Repositories;
+using Data.Interfaces;
+
+namespace EventsApp.Droid
+{
+    [Activity(Label = "EventsApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            var dbPath =
+                Path.Combine(System.Environment.GetFolderPath
+                (System.Environment.SpecialFolder.Personal), "eventsDB.db");
+
+            IEventsRepository eventsRepository = new EventsRepository(dbPath);
+            IOrganisationRepository organisationRepository = new OrganisationRepository(dbPath);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App(eventsRepository, organisationRepository));
+          
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
